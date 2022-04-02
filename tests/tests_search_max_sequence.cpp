d59027347c066e122f752d0533f67090dc2e64e1
Polina Yakimkina <bvt3rfl1@gmail.com>
2022-04-02 09:06:57 +0300

    add1

--- tests/tests_search_max_sequence.cpp
+++ tests/tests_search_max_sequence.cpp
@@ -15,16 +15,17 @@ TEST(TestBasic, 1)
 {
 	char *max = search_max_sequence_single_thread("../tests/tests_sample/testBasic1.txt", 7);
 	printf("%s\n", max);
-	free(max);
 
 	EXPECT_EQ(strcmp(max, "123"), 0);
+	free(max);
+
 }
 
 TEST(TestBasic, 2)
 {
 	char *max = search_max_sequence_single_thread("../tests/tests_sample/testBasic2.txt", 17);
 	printf("%s\n", max);
-	free(max);
 
 	EXPECT_EQ(strcmp(max, "111111111"), 0);
+	free(max);
 }
